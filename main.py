109f74aae78384de41ad8f2cdf18865fb5863a56
changmoaaa <153179707+changmoaaa@users.noreply.github.com>
2025-04-03 21:24:00 +0800

    main.py

--- main.py
+++ main.py
@@ -301,6 +301,26 @@ if __name__ == "__main__":
         # 获取词霸每日金句
         note_ch, note_en = get_ciba()
     chp = get_tianhang()
+def get_daily_love():
+    # 每日一句情话
+    url = "https://api.lovelive.tools/api/SweetNothings/Serialization/Json"
+    r = requests.get(url)
+    all_dict = json.loads(r.text)
+    sentence = all_dict['returnObj'][0]
+    daily_love = sentence
+    return daily_love
+def get_ciba():
+    # 每日一句英语（来自爱词霸）
+    url = "http://open.iciba.com/dsapi/"
+    r = get(url)
+    note_en = r.json()["content"]
+    note_ch = r.json()["note"]
+    return note_ch, note_en
+ # 获取每日情话
+    daily_love = get_daily_love()
+    # 获取每日一句英语
+    note_qch, note_qen = get_ciba()
+
     # 公众号推送消息
     for user in users:
         send_message(user, accessToken, region, weather, temp, wind_dir, note_ch, note_en, max_temp, min_temp, sunrise,
